5a176b70b2a9fc06d754d425894e54381eee5d5d
Forven <forven47@outlook.com>
2026-01-10 15:20:22 +0800

    change webui pictrue

--- module/webui/setting.py
+++ module/webui/setting.py
@@ -61,6 +61,8 @@ class State:
     theme: str = "default"
     last_screenshot_base64: str = None
     placeholder_images: list = [
+        "screen0.jpg",
+        '''
         "screen1.jpg",
         "screen2.jpg",
         "screen3.jpg",
@@ -70,6 +72,7 @@ class State:
         "screen7.png",
         "screen8.jpg",
         "screen9.png",
+        '''
     ]
     placeholder_index: int = 0
 
